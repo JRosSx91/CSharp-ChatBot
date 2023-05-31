309723f9b3007b04fbac6e20bee9a788619a410d
JRosSx91 <hazee.1491@gmail.com>
2023-05-31 18:10:20 +0200

    fix issues

--- ChatApp/Program.cs
+++ ChatApp/Program.cs
@@ -7,30 +7,59 @@ namespace ChatApp
 {
     class Program
     {
+        private static readonly HttpClient client = new HttpClient();
+
         static async Task Main(string[] args)
         {
-            DotNetEnv.Env.Load();
-
-            var openAi = new OpenAIClient(Environment.GetEnvironmentVariable("OPENAI_KEY"));
-
             while (true)
             {
                 Console.Write("Enter your message (type 'exit' to quit): ");
                 var userMessage = Console.ReadLine();
 
+                if (userMessage is null)
+                {
+                    Console.WriteLine("Invalid input. Please try again.");
+                    continue;
+                }
+
                 if (userMessage.Trim().ToLower() == "exit")
                     break;
 
-                var response = await ChatGpt3(openAi, userMessage);
+                var response = await ChatWithGpt3(userMessage);
 
                 Console.WriteLine("GPT-3 Response: " + response);
             }
         }
 
-        static async Task<string> ChatGpt3(OpenAIClient openAi, string userMessage)
+        static async Task<string> ChatWithGpt3(string userMessage)
         {
-            var completion = await openAi.CompleteConversationAsync(new[] { new ChatMessage { role = "user", content = userMessage } });
-            return completion.choices[0].message.content;
+            var openAiUrl = "https://api.openai.com/v1/chat/completions";
+            var openAiApiKey = "<tu_clave_de_API>";
+
+            var requestBody = $"{{\"model\":\"gpt-3.5-turbo\",\"messages\":[{{\"role\":\"user\",\"content\":\"{userMessage}\"}}]}}";
+
+            var requestContent = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {openAiApiKey}");
+
+            var response = await client.PostAsync(openAiUrl, requestContent);
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            return ParseGpt3Response(responseContent);
+        }
+
+        static string ParseGpt3Response(string responseContent)
+        {
+            // Implementa el código para extraer la respuesta de la API de OpenAI según el formato de respuesta específico que recibas
+            // Puedes usar la biblioteca Newtonsoft.Json para analizar el JSON de la respuesta si es necesario.
+            // Aquí hay un ejemplo básico:
+
+            dynamic responseObject = Newtonsoft.Json.JsonConvert.DeserializeObject(responseContent);
+            string gpt3Response = responseObject.choices[0].message.content;
+
+            return gpt3Response;
         }
     }
 }
